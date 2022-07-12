dd2568f642f2fa02e766f9f9823ff9617a94e687
sawthiha <sawthiha@mtu.edu.mm>
2022-07-12 18:15:59 +0900

    Facial Movement Detection is now 1-to-1!

--- face_activity/face_movement_calculator.cc
+++ face_activity/face_movement_calculator.cc
@@ -7,7 +7,6 @@
 #include "mediapipe/framework/port/status.h"
 #include "mediapipe/framework/port/opencv_core_inc.h"
 #include "mediapipe/framework/formats/landmark.pb.h"
-#include "mediapipe/calculators/core/begin_loop_calculator.h"
 
 namespace mediapipe
 {
@@ -15,16 +14,16 @@ namespace mediapipe
      * @brief Detect face position changes on screen
      * 
      * INPUTS:
-     *      0 - Landmarks (std::vector<NormalizedLandmarkList>)
+     *      0 - Landmarks (NormalizedLandmarkList)
      * OUTPUTS:
-     *      0 - Face Position Deltas (std::vector<double> )
+     *      0 - Face Position Delta (double)
      * 
      * Example:
      * 
      * node {
      *   calculator: "FaceMovementCalculator"
-     *   input_stream: "multi_face_landmarks"
-     *   output_stream: "multi_face_movements"
+     *   input_stream: "face_landmarks"
+     *   output_stream: "face_movement"
      * }
      * 
      */
@@ -49,8 +48,8 @@ namespace mediapipe
 
     absl::Status FaceMovementCalculator::GetContract(CalculatorContract* cc)
     {
-        cc->Inputs().Index(0).Set<std::vector<NormalizedLandmarkList> >();
-        cc->Outputs().Index(0).Set<std::vector<double> >();
+        cc->Inputs().Index(0).Set<NormalizedLandmarkList>();
+        cc->Outputs().Index(0).Set<double>();
         return absl::OkStatus();
     }
 
@@ -59,20 +58,13 @@ namespace mediapipe
 
     absl::Status FaceMovementCalculator::Process(CalculatorContext* cc)
     {
-        std::vector<double > multi_face_movements;
-        if (!cc->Inputs().Index(0).IsEmpty())
-        {
-            const auto& multi_face_landmarks = cc->Inputs().Index(0).Get<std::vector<NormalizedLandmarkList> >();
-            for(auto&& landmarks: multi_face_landmarks)
-            {
-                auto cur_landmark = landmarks.landmark(0);
-                cv::Vec3f cur_vec (cur_landmark.x(), cur_landmark.y(), cur_landmark.z());
-                multi_face_movements.push_back(cv::norm(cur_vec - m_prev_vec, cv::NORM_L2));
-                m_prev_vec = cur_vec;
-            }
-        }
+        auto landmarks = cc->Inputs().Index(0).Get<NormalizedLandmarkList>();
+        auto cur_landmark = landmarks.landmark(0);
+        cv::Vec3f cur_vec (cur_landmark.x(), cur_landmark.y(), cur_landmark.z());
+        auto delta = cv::norm(cur_vec - m_prev_vec, cv::NORM_L2);
+        m_prev_vec = cur_vec;
             
-        Packet packet = MakePacket<decltype(multi_face_movements)>(multi_face_movements).At(cc->InputTimestamp());
+        Packet packet = MakePacket<decltype(delta)>(delta).At(cc->InputTimestamp());
         cc->Outputs().Index(0).AddPacket(packet);
 
         return absl::OkStatus();
@@ -80,10 +72,5 @@ namespace mediapipe
 
     absl::Status FaceMovementCalculator::Close(CalculatorContext* cc)
     { return absl::OkStatus(); }
-
-    typedef BeginLoopCalculator<std::vector<double > >
-        BeginLoopFaceMovementVectorCalculator;
-    // Register the begin loop calculator
-    REGISTER_CALCULATOR(BeginLoopFaceMovementVectorCalculator);
 
 } // namespace mediapipe
