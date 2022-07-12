baa502cda59636b1b4f6616afb49a20b7f14b376
sawthiha <sawthiha@mtu.edu.mm>
2022-07-12 18:40:49 +0900

    ProctorResult is now 1-to-1!

--- util/proctor_result_calculator.cc
+++ util/proctor_result_calculator.cc
@@ -8,19 +8,17 @@ namespace mediapipe
      * @brief Proctor Result Calculator
      * 
      * OUTPUTS:
-     *      RESULT - Proctoring Result <std::vector<ProctorResult>>
+     *      RESULT - Proctoring Result <ProctorResult>
      * 
      * Example:
      * 
      * # Proctor Result Calculator
      *  node  {
      *      calculator: "ProctorResultCalculator"
-     *      input_stream: "TICK:sync_stream"
-     *      input_stream: "THROTTLE:throttle_stream"
-     *      input_stream: "ALIGN:multi_face_alignments"
-     *      input_stream: "BLINK:multi_face_blinks"
-     *      input_stream: "ACTIVE:multi_face_activities"
-     *      input_stream: "MOVE:multi_face_movements"
+     *      input_stream: "ALIGN:face_alignments"
+     *      input_stream: "BLINK:face_blinks"
+     *      input_stream: "ACTIVE:face_activity"
+     *      input_stream: "MOVE:face_movement"
      *      output_stream: "RESULT:result"
      * 
      *  }
@@ -28,9 +26,6 @@ namespace mediapipe
      */
     class ProctorResultCalculator: public CalculatorBase
     {
-
-    private:
-        Timestamp m_last_timestamp = Timestamp::Unset();
 
     public:
         ProctorResultCalculator() = default;
@@ -48,16 +43,11 @@ namespace mediapipe
 
     absl::Status ProctorResultCalculator::GetContract(CalculatorContract* cc)
     {
-        // cc->Inputs().Tag("TICK").SetAny();
-        cc->Inputs().Tag("THROTTLE").SetAny();
-        cc->Inputs().Tag("ALIGN").Set<std::vector<std::map<std::string, double> >>();
-        cc->Inputs().Tag("BLINK").Set<std::vector<std::map<std::string, double> >>();
-        cc->Inputs().Tag("ACTIVE").Set<std::vector<double>>();
-        cc->Inputs().Tag("MOVE").Set<std::vector<double>>();
-        cc->Outputs().Tag("RESULT").Set<std::vector<ProctorResult>>();
-
-        cc->SetInputStreamHandler("ImmediateInputStreamHandler");
-        // cc->SetProcessTimestampBounds(true);
+        cc->Inputs().Tag("ALIGN").Set<std::map<std::string, double>>();
+        cc->Inputs().Tag("BLINK").Set<std::map<std::string, double>>();
+        cc->Inputs().Tag("ACTIVE").Set<double>();
+        cc->Inputs().Tag("MOVE").Set<double>();
+        cc->Outputs().Tag("RESULT").Set<ProctorResult>();
 
         return absl::OkStatus();
     }
@@ -69,45 +59,21 @@ namespace mediapipe
 
     absl::Status ProctorResultCalculator::Process(CalculatorContext* cc)
     {
-        // if(m_last_timestamp == Timestamp::Unset())
-        // {
-        //     m_last_timestamp = 
-        // }
-        std::vector<ProctorResult> results;
-        // auto is_face_detected = cc->Inputs().Tag("ALIGN").IsEmpty() || ;
-        // auto alignments =  ? std::vector<std::map<std::string, double> >()
-        //     : cc->Inputs().Tag("ALIGN").Get<std::vector<std::map<std::string, double> >>();
-        // auto blinks = cc->Inputs().Tag("BLINK").IsEmpty() ? std::vector<std::map<std::string, double> >()
-        //     : cc->Inputs().Tag("BLINK").Get<std::vector<std::map<std::string, double> >>();
-        // auto activities = cc->Inputs().Tag("ACTIVE").IsEmpty() ? 
-        // cc->Inputs().Tag("ACTIVE").Get<std::vector<double>>();
-        // auto movements = cc->Inputs().Tag("MOVE").Get<std::vector<double>>();
-
-        // for (size_t i = 0; i < alignments.size(); i++)
-        // {
-        //     ProctorResult result;
-        //     auto blink = blinks.at(i);
-        //     auto threshold = blink.at("threshold");
-        //     result.is_left_eye_blinking = blink.at("left") < threshold;
-        //     result.is_right_eye_blinking = blink.at("right") < threshold;
+        ProctorResult result;
+        auto blink = cc->Inputs().Tag("BLINK").Get<std::map<std::string, double>>();
+        auto threshold = blink.at("threshold");
+        result.is_left_eye_blinking = blink.at("left") < threshold;
+        result.is_right_eye_blinking = blink.at("right") < threshold;
+        
+        auto alignment = cc->Inputs().Tag("ALIGN").Get<std::map<std::string, double>>();
+        result.horizontal_align = alignment.at("horizontal_align");
+        result.vertical_align   = alignment.at("vertical_align");
             
-        //     auto alignment = alignments.at(i);
-        //     result.horizontal_align = alignment.at("horizontal_align");
-        //     result.vertical_align   = alignment.at("vertical_align");
-            
-        //     result.facial_activity = activities.at(i);
-        //     result.face_movement = movements.at(i);
+        result.facial_activity = cc->Inputs().Tag("ACTIVE").Get<double>();
+        result.face_movement = cc->Inputs().Tag("MOVE").Get<double>();
 
-        //     results.push_back(std::move(result));
-        // }
-
-        auto tick = cc->Inputs().Tag("THROTTLE").IsEmpty();
-        auto tick_packet = cc->Inputs().Tag("THROTTLE").Value();
-        std::cout <<  "Throttle Timestamp: " << tick_packet.Timestamp().NextAllowedInStream().Value() << std::endl;
-
-        // Packet packet = MakePacket<decltype(results)>(results)
-        //     .At(tick_packet.Timestamp()); 
-        // cc->Outputs().Tag("RESULT").AddPacket(packet);
+        Packet packet = MakePacket<decltype(result)>(result).At(cc->InputTimestamp()); 
+        cc->Outputs().Tag("RESULT").AddPacket(packet);
 
         return absl::OkStatus();
     } // Process()
