623d72202915948e436836c9664f2e114e5f377e
sawthiha <sawthiha@mtu.edu.mm>
2022-07-12 18:02:23 +0900

    LandmarkStandardization is now 1-to-1!

--- util/landmark_standardization.cc
+++ util/landmark_standardization.cc
@@ -22,9 +22,8 @@ namespace mediapipe
      * 
      * node {
      *   calculator: "LandmarkStandardizationCalculator"
-     *   input_stream: "IMAGE:throttled_input_video"
-     *   input_stream: "multi_face_landmarks"
-     *   output_stream: "multi_face_std_landmarks"
+     *   input_stream: "face_landmarks"
+     *   output_stream: "face_std_landmarks"
      * }
      * 
      */
@@ -46,50 +45,43 @@ namespace mediapipe
 
     absl::Status LandmarkStandardizationCalculator::GetContract(CalculatorContract* cc)
     {
-        cc->Inputs().Tag("IMAGE").SetAny();
-        cc->Inputs().Index(0).Set<std::vector<NormalizedLandmarkList> >();
-        cc->Outputs().Index(0).Set<std::vector<NormalizedLandmarkList> >();
+        cc->Inputs().Index(0).Set<NormalizedLandmarkList>();
+        cc->Outputs().Index(0).Set<NormalizedLandmarkList>();
         return absl::OkStatus();
     }
 
     absl::Status LandmarkStandardizationCalculator::Open(CalculatorContext* cc)
-    { return absl::OkStatus(); }
+    {
+        return absl::OkStatus();
+    }
 
     absl::Status LandmarkStandardizationCalculator::Process(CalculatorContext* cc)
     {
-        std::vector<NormalizedLandmarkList> multi_norm_landmarks;
-        if (!cc->Inputs().Index(0).IsEmpty())
-        {
-            const auto& multiface_landmarks = cc->Inputs().Index(0).Get<std::vector<NormalizedLandmarkList> >();
-            for(auto&& landmarks: multiface_landmarks)
-            {
-                cv::Mat mat(landmarks.landmark_size(), 3, CV_64FC1);
-                cv::Mat norm_mat(landmarks.landmark_size(), 3, CV_64FC1);
-                cv::Mat mean_mat, std_mat;
+        auto landmarks = cc->Inputs().Index(0).Get<NormalizedLandmarkList>();
+        cv::Mat mat(landmarks.landmark_size(), 3, CV_64FC1);
+        cv::Mat norm_mat(landmarks.landmark_size(), 3, CV_64FC1);
+        cv::Mat mean_mat, std_mat;
 
-                for (int i = 0; i < landmarks.landmark_size(); ++i) {
-                    mat.at<double>(i, 0) = landmarks.landmark(i).x();
-                    mat.at<double>(i, 1) = landmarks.landmark(i).y();
-                    mat.at<double>(i, 2) = landmarks.landmark(i).z();
-                }
-
-                for (int i = 0; i < mat.cols; i++) {
-                    cv::meanStdDev(mat.col(i), mean_mat, std_mat);
-                    norm_mat.col(i) = (mat.col(i) - mean_mat.at<double>(0, 0)) / std_mat.at<double>(0, 0);
-                }
-
-                NormalizedLandmarkList norm_landmarks;
-                for (int i = 0; i < landmarks.landmark_size(); ++i) {
-                    NormalizedLandmark* landmark = norm_landmarks.add_landmark();
-                    landmark->set_x(norm_mat.at<double>(i, 0));
-                    landmark->set_y(norm_mat.at<double>(i, 1));
-                    landmark->set_z(norm_mat.at<double>(i, 2));
-                }
-                multi_norm_landmarks.push_back(norm_landmarks);
-            }
+        for (int i = 0; i < landmarks.landmark_size(); ++i) {
+            mat.at<double>(i, 0) = landmarks.landmark(i).x();
+            mat.at<double>(i, 1) = landmarks.landmark(i).y();
+            mat.at<double>(i, 2) = landmarks.landmark(i).z();
         }
 
-        Packet packet = MakePacket<decltype(multi_norm_landmarks)>(multi_norm_landmarks).At(cc->InputTimestamp());
+        for (int i = 0; i < mat.cols; i++) {
+            cv::meanStdDev(mat.col(i), mean_mat, std_mat);
+            norm_mat.col(i) = (mat.col(i) - mean_mat.at<double>(0, 0)) / std_mat.at<double>(0, 0);
+        }
+
+        NormalizedLandmarkList norm_landmarks;
+        for (int i = 0; i < landmarks.landmark_size(); ++i) {
+            NormalizedLandmark* landmark = norm_landmarks.add_landmark();
+            landmark->set_x(norm_mat.at<double>(i, 0));
+            landmark->set_y(norm_mat.at<double>(i, 1));
+            landmark->set_z(norm_mat.at<double>(i, 2));
+        }
+
+        Packet packet = MakePacket<decltype(norm_landmarks)>(norm_landmarks).At(cc->InputTimestamp());
         cc->Outputs().Index(0).AddPacket(packet);
 
         return absl::OkStatus();
--- util/proctor_result_calculator.cc
+++ util/proctor_result_calculator.cc
@@ -16,16 +16,21 @@ namespace mediapipe
      *  node  {
      *      calculator: "ProctorResultCalculator"
      *      input_stream: "TICK:sync_stream"
+     *      input_stream: "THROTTLE:throttle_stream"
      *      input_stream: "ALIGN:multi_face_alignments"
      *      input_stream: "BLINK:multi_face_blinks"
      *      input_stream: "ACTIVE:multi_face_activities"
      *      input_stream: "MOVE:multi_face_movements"
      *      output_stream: "RESULT:result"
+     * 
      *  }
      * 
      */
     class ProctorResultCalculator: public CalculatorBase
     {
+
+    private:
+        Timestamp m_last_timestamp = Timestamp::Unset();
 
     public:
         ProctorResultCalculator() = default;
@@ -43,12 +48,17 @@ namespace mediapipe
 
     absl::Status ProctorResultCalculator::GetContract(CalculatorContract* cc)
     {
-        cc->Inputs().Tag("TICK").SetAny();
+        // cc->Inputs().Tag("TICK").SetAny();
+        cc->Inputs().Tag("THROTTLE").SetAny();
         cc->Inputs().Tag("ALIGN").Set<std::vector<std::map<std::string, double> >>();
         cc->Inputs().Tag("BLINK").Set<std::vector<std::map<std::string, double> >>();
         cc->Inputs().Tag("ACTIVE").Set<std::vector<double>>();
         cc->Inputs().Tag("MOVE").Set<std::vector<double>>();
         cc->Outputs().Tag("RESULT").Set<std::vector<ProctorResult>>();
+
+        cc->SetInputStreamHandler("ImmediateInputStreamHandler");
+        // cc->SetProcessTimestampBounds(true);
+
         return absl::OkStatus();
     }
 
@@ -59,33 +69,45 @@ namespace mediapipe
 
     absl::Status ProctorResultCalculator::Process(CalculatorContext* cc)
     {
-
+        // if(m_last_timestamp == Timestamp::Unset())
+        // {
+        //     m_last_timestamp = 
+        // }
         std::vector<ProctorResult> results;
-        auto alignments = cc->Inputs().Tag("ALIGN").Get<std::vector<std::map<std::string, double> >>();
-        auto blinks = cc->Inputs().Tag("BLINK").Get<std::vector<std::map<std::string, double> >>();
-        auto activities = cc->Inputs().Tag("ACTIVE").Get<std::vector<double>>();
-        auto movements = cc->Inputs().Tag("MOVE").Get<std::vector<double>>();
+        // auto is_face_detected = cc->Inputs().Tag("ALIGN").IsEmpty() || ;
+        // auto alignments =  ? std::vector<std::map<std::string, double> >()
+        //     : cc->Inputs().Tag("ALIGN").Get<std::vector<std::map<std::string, double> >>();
+        // auto blinks = cc->Inputs().Tag("BLINK").IsEmpty() ? std::vector<std::map<std::string, double> >()
+        //     : cc->Inputs().Tag("BLINK").Get<std::vector<std::map<std::string, double> >>();
+        // auto activities = cc->Inputs().Tag("ACTIVE").IsEmpty() ? 
+        // cc->Inputs().Tag("ACTIVE").Get<std::vector<double>>();
+        // auto movements = cc->Inputs().Tag("MOVE").Get<std::vector<double>>();
 
-        for (size_t i = 0; i < alignments.size(); i++)
-        {
-            ProctorResult result;
-            auto blink = blinks.at(i);
-            auto threshold = blink.at("threshold");
-            result.is_left_eye_blinking = blink.at("left") < threshold;
-            result.is_right_eye_blinking = blink.at("right") < threshold;
+        // for (size_t i = 0; i < alignments.size(); i++)
+        // {
+        //     ProctorResult result;
+        //     auto blink = blinks.at(i);
+        //     auto threshold = blink.at("threshold");
+        //     result.is_left_eye_blinking = blink.at("left") < threshold;
+        //     result.is_right_eye_blinking = blink.at("right") < threshold;
             
-            auto alignment = multi_face_alignments.at(i);
-            result.horizontal_align = alignment.at("horizontal_align");
-            result.vertical_align   = alignment.at("vertical_align");
+        //     auto alignment = alignments.at(i);
+        //     result.horizontal_align = alignment.at("horizontal_align");
+        //     result.vertical_align   = alignment.at("vertical_align");
             
-            result.facial_activity = activities.at(i);
-            result.face_movement = movements.at(i);
+        //     result.facial_activity = activities.at(i);
+        //     result.face_movement = movements.at(i);
 
-            results.push_back(std::move(result));
-        }
+        //     results.push_back(std::move(result));
+        // }
 
-        Packet packet = MakePacket<decltype(results)>(results).At(cc->InputTimestamp());
-        cc->Outputs().Tag("RESULT").AddPacket(packet);
+        auto tick = cc->Inputs().Tag("THROTTLE").IsEmpty();
+        auto tick_packet = cc->Inputs().Tag("THROTTLE").Value();
+        std::cout <<  "Throttle Timestamp: " << tick_packet.Timestamp().NextAllowedInStream().Value() << std::endl;
+
+        // Packet packet = MakePacket<decltype(results)>(results)
+        //     .At(tick_packet.Timestamp()); 
+        // cc->Outputs().Tag("RESULT").AddPacket(packet);
 
         return absl::OkStatus();
     } // Process()
