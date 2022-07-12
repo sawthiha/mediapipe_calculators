e2453c6a7ff786666e0e4924f7905a6393369d0e
sawthiha <sawthiha@mtu.edu.mm>
2022-07-12 18:18:59 +0900

    Alignment Detection is now 1-to-1!

--- face_alignment/face_alignment_calculator.cc
+++ face_alignment/face_alignment_calculator.cc
@@ -5,7 +5,6 @@
 #include "mediapipe/framework/calculator_framework.h"
 #include "mediapipe/framework/port/status.h"
 #include "mediapipe/framework/formats/landmark.pb.h"
-#include "mediapipe/calculators/core/begin_loop_calculator.h"
 
 namespace mediapipe
 {
@@ -13,9 +12,9 @@ namespace mediapipe
      * @brief Detect face alignments from Standardized Landmarks
      * 
      * INPUTS:
-     *      0 - Standardized Landmarks (std::vector<NormalizedLandmarkList>)
+     *      0 - Standardized Landmarks (NormalizedLandmarkList)
      * OUTPUTS:
-     *      0 - Face Alignment data (std::vector<std::map<std::string, double> >)
+     *      0 - Face Alignment data (std::map<std::string, double>)
      *      {
      *          "horizontal_align": 0.0 being neutral, + being right, - being left
      *          "vertical_align":   0.0 being neutral, + being down,  - being up
@@ -25,8 +24,8 @@ namespace mediapipe
      * 
      * node {
      *   calculator: "FaceAlignmentCalculator"
-     *   input_stream: "multi_face_std_landmarks"
-     *   output_stream: "multi_face_alignments"
+     *   input_stream: "face_std_landmarks"
+     *   output_stream: "face_alignments"
      * }
      * 
      */
@@ -49,8 +48,8 @@ namespace mediapipe
 
     absl::Status FaceAlignmentCalculator::GetContract(CalculatorContract* cc)
     {
-        cc->Inputs().Index(0).Set<std::vector<NormalizedLandmarkList> >();
-        cc->Outputs().Index(0).Set<std::vector<std::map<std::string, double> > >();
+        cc->Inputs().Index(0).Set<NormalizedLandmarkList>();
+        cc->Outputs().Index(0).Set<std::map<std::string, double>>();
         return absl::OkStatus();
     }
 
@@ -59,20 +58,12 @@ namespace mediapipe
 
     absl::Status FaceAlignmentCalculator::Process(CalculatorContext* cc)
     {
-        std::vector<std::map<std::string, double> > multi_face_alignments;
-        if (!cc->Inputs().Index(0).IsEmpty())
-        {
-            const auto& multi_face_landmarks = cc->Inputs().Index(0).Get<std::vector<NormalizedLandmarkList> >();
-            for(auto&& landmarks: multi_face_landmarks)
-            {
-                std::map<std::string, double> alignment_map;
-                alignment_map["horizontal_align"]   = landmarks.landmark(1).x();
-                alignment_map["vertical_align"]     = landmarks.landmark(1).y();
-                multi_face_alignments.push_back(alignment_map);
-            }
-        }
+        auto landmarks = cc->Inputs().Index(0).Get<NormalizedLandmarkList>();
+        std::map<std::string, double> alignment_map;
+        alignment_map["horizontal_align"]   = landmarks.landmark(1).x();
+        alignment_map["vertical_align"]     = landmarks.landmark(1).y();
             
-        Packet packet = MakePacket<decltype(multi_face_alignments)>(multi_face_alignments).At(cc->InputTimestamp());
+        Packet packet = MakePacket<decltype(alignment_map)>(alignment_map).At(cc->InputTimestamp());
         cc->Outputs().Index(0).AddPacket(packet);
 
         return absl::OkStatus();
@@ -80,10 +71,5 @@ namespace mediapipe
 
     absl::Status FaceAlignmentCalculator::Close(CalculatorContext* cc)
     { return absl::OkStatus(); }
-
-    typedef BeginLoopCalculator<std::vector<std::map<std::string, double> > >
-        BeginLoopAlignmentVectorCalculator;
-    // Register the begin loop calculator
-    REGISTER_CALCULATOR(BeginLoopAlignmentVectorCalculator);
 
 } // namespace mediapipe
